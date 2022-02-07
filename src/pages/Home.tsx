2b28288879ca3d4f0325b086f578f2eb56215513
lee jumi <a25320@naver.com>
2022-02-07 16:35:38 +0900

    날짜 적용한 리스트

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,10 +3,23 @@ import { DiaryStateContext } from '../App';
 import { Header, Button, DiaryList } from '../components';
 
 export const Home = () => {
-  const data = useContext(DiaryStateContext);
+  const list = useContext(DiaryStateContext);
+
+  const [data, setData] = useState([]);
   const [day, setDay] = useState(new Date());
   const header_text = `${day.getFullYear()}년 ${day.getMonth()+1}월`;
-  
+
+  useEffect(() => {
+    if (list.length >= 1) {
+      const firstDay = new Date(day.getFullYear(), day.getMonth(), 1).getTime();
+      const lastDay = new Date(day.getFullYear(), day.getMonth()+1, 0, 23, 59, 59).getTime();
+
+      setData(
+        list.filter((el: any) => Date.parse(el.date) >= firstDay && Date.parse(el.date) <= lastDay)
+      );
+    }
+  }, [list, day]);
+
   // 저번달
   const prevMonth = () => {
     setDay(
@@ -28,7 +41,7 @@ export const Home = () => {
         right={<Button text={'▶'} type='default' onClick={() => nextMonth()} />}
         text={header_text}
       />
-      <DiaryList list={data} />
+      { data && <DiaryList list={data} /> }
     </div>
   );
 }
